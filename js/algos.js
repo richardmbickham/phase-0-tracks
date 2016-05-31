5c83225c5190218ce591e592c32b67276a527fba
Richard Bickham <Rich@Richards-MacBook-Air.local>
2016-05-31 08:56:36 -0700

    Pseudocode for Release 0

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+//Pseudocode
+//Write a function that takes an array as an arguement
+  //iterate over the array to determine which the longest string
+//set variable = 0 
+//for each item in the array determine length of string
+// print item with longest string length
